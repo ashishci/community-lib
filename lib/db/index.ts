0a3cdea5087448b77623ee98b5aa5d1fec9a2118
Ash Chatterjee <ashish@ashishc.io>
2023-03-04 00:19:30 -0800

    reconfiguring structure

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,16 +1,20 @@
 import { Pool, PoolConfig } from 'pg'
 
-import { LoggerPartial } from '../interfaces';
+import { LoggerPartial } from '../interfaces'
 
-export const connectDb = async (sql: string, dbConfig: PoolConfig, logger: LoggerPartial) => {
-    try {
-        const pool = new Pool(dbConfig as PoolConfig);
+export const connectDb = async (
+  sql: string,
+  dbConfig: PoolConfig,
+  logger: LoggerPartial
+) => {
+  try {
+    const pool = new Pool(dbConfig as PoolConfig)
 
-        await pool.connect()
-        const res = await pool.query(sql)
-        logger.info(res)
-        await pool.end()
-    } catch (error) {
-        logger.error(error)
-    }
+    await pool.connect()
+    const res = await pool.query(sql)
+    logger.info(res)
+    await pool.end()
+  } catch (error) {
+    logger.error(error)
+  }
 }
